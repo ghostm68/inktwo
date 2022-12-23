ff4ae8580f5a78cbe48bd56cb3d5d6cb76871195
ghostm68 <42044862+ghostm68@users.noreply.github.com>
2022-12-23 03:40:00 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+// Installing service worker
+const CACHE_NAME = 'https://www.authorwave.gq/';
+
+/* Add relative URL of all the static content you want to store in
+* cache storage (this will help us use our app offline)*/
+let resourcesToCache = ["./ghostm68/Parallax-RellaxJav/tree/master/Images", ".VAPORGRAM1609365711615.mp4", "./rellax.min.js", "./style.css"];
+
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache =>{
+return cache.addAll(resourcesToCache);
+})
+);
+});
+// Cache and return requests
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(response=>{
+return response || fetch(e.request);
+})
+);
+});
+
+// Update a service worker
+const cacheWhitelist = ['https://authorwave.gq/'];
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(cacheNames => {
+return Promise.all(
+cacheNames.map(cacheName => {
+if (cacheWhitelist.indexOf(cacheName) === -1) {
+return caches.delete(cacheName);
+}
+})
+);
+})
+);
+});
