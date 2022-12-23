ac9634df3cecd77930bbd6303c35bebdccc36f55
ghostm68 <42044862+ghostm68@users.noreply.github.com>
2022-12-23 03:13:41 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 // Installing service worker
-const CACHE_NAME = 'https://www.authorwave.gq/';
+const CACHE_NAME = 'https://www.inkrealm.info/';
 
 /* Add relative URL of all the static content you want to store in
 * cache storage (this will help us use our app offline)*/
-let resourcesToCache = ["./ghostm68/Parallax-RellaxJav/tree/master/Images", ".VAPORGRAM1609365711615.mp4", "./rellax.min.js", "./style.css"];
+let resourcesToCache = ["./ghostm68/inktwo/tree/master/Images", ".VAPORGRAM1609365711615.mp4", "./rellax.min.js", "./style.css"];
 
 self.addEventListener("install", e=>{
 e.waitUntil(
