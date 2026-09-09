3545522edd4169b17b72ac5e102571d6f4d32809
inky <42044862+ghostm68@users.noreply.github.com>
2026-09-09 13:03:52 -0400

    Fix formatting and capitalization in cover data

--- js/616.js
+++ js/616.js
@@ -763,12 +763,12 @@ const coverData = [
     src: "armaggedon two skinny girls.mp3",
     feed: "ARMA6EDDON // hellbender"
   },
-     {
+ {
     tag: "COVER // 06. Some Like it Hot",
     title: "Some like it hot (Two Skinny 6irls)",
     desc: "orchestrated recording. film score.",
     src: "some like it alt m.mp3",
-    feed: "SOme Like it Hot // ost"
+    feed: "Some Like it Hot // ost"
   }
 ];
 
